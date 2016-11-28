15000bd881c582c1dd88e1bc86c6759a206c5889
Felicity <chobytes@icloud.com>
2016-11-27 23:41:03 -0600

    refactored Nat, Bool

--- main2.hs
+++ main2.hs
@@ -13,23 +13,10 @@
 
 import Foundation
 
-class Show n => Numeric n where
-  zero :: n
-  one :: n
-  two :: n
-  three :: n
-  four :: n
-  five :: n
-  six :: n
-  seven :: n
-  eight :: n
-  nine :: n
+
 
 class Eval ty where
   eval :: ty -> ty
-
-class Equality' ty where
-  isEq :: ty -> ty -> Boolean
 
 data family Expression ty :: *
 
@@ -41,34 +28,9 @@ data INat :: *
 deriving instance Show INat
 type Nat = Expression INat
 
--- instance Numeric (INat) where
---   zero = Z
---   one = S zero
---   two = S one
---   three = S two
---   four = S three
---   five = S four
---   six = S five
---   seven = S six
---   eight = S seven
---   nine = S eight
-
--- instance Numeric (Nat) where
---   zero = Nat (zero :: INat)
---   one = Nat (one :: INat)
---   two = Nat (two :: INat)
---   three = Nat (three :: INat)
---   four = Nat (four :: INat)
---   five = Nat (five :: INat)
---   six = Nat (six :: INat)
---   seven = Nat (seven :: INat)
---   eight = Nat (eight :: INat)
---   nine = Nat (nine :: INat)
-
 data instance Expression INat where
   Z :: Nat
   S :: Nat -> Nat
---  Nat :: INat -> Nat
   Succ :: Nat -> Nat
   Pred :: Nat -> Nat
   Add :: Nat -> Nat -> Nat
@@ -102,87 +64,44 @@ instance Eval Nat where
           mult a Z acc = acc
           mult a (S b) acc = mult a b (eval $ Add a acc)
 
--- instance Equality' Nat where
---   isEq (Nat Z) (Nat Z) = Bool T
---   isEq (Nat (S a)) (Nat (S b)) = isEq (Nat a) (Nat b)
---   isEq _ _ = Bool F
 
 
+data IBoolean :: *
+deriving instance (Show (IBoolean)) => Show (Expression (IBoolean))
+type Boolean = Expression (IBoolean)
 
--- Booleans
-
-data IBoolean = T | F deriving Show
-type Boolean = Expression IBoolean
-data instance Expression IBoolean where
-  Bool :: IBoolean -> Boolean
+data instance Expression (IBoolean) where
+  T :: Boolean
+  F :: Boolean
   Or :: Boolean -> Boolean -> Boolean
   And :: Boolean -> Boolean -> Boolean
   Not :: Boolean -> Boolean
   XOr :: Boolean -> Boolean -> Boolean
-  NOr :: Boolean -> Boolean -> Boolean
-deriving instance Show IBoolean => Show (Expression IBoolean)
-
-instance Equality' Boolean where
-  isEq (Bool T) (Bool T) = Bool T
-  isEq (Bool F) (Bool F) = Bool T
-  isEq _ _ = Bool F
-
-instance Eval Boolean where
-  eval a@(Bool _) = a
-
-
-
--- Helpers
-
-evalTerm2 :: Eval t => t -> t -> (t, t)
-evalTerm2 t1 t2 = (eval t1, eval t2)
-
-
-
--- For testing purposes
--- interact with functions on Nats using "normal" numbers
--- eg add 2 2 vs (Add (S (S Z)) (S (S Z)))
-
--- fromNat (Nat n) = fromNat' n 0
---   where fromNat' :: INat -> Int -> Int
---         fromNat' Z acc = acc
---         fromNat' (S a) acc = fromNat' a (acc + 1)
-
--- fromInt :: Int -> Nat
--- fromInt n = fromInt' n Z
---   where fromInt' :: Int -> INat -> Nat
---         fromInt' 0 acc = Nat acc
---         fromInt' a acc = fromInt' (a - 1) (S acc)
-
--- natExp :: (Nat -> Nat -> Nat) -> Int -> Int -> Int
--- natExp f a b = fromNat . eval $ f (fromInt a) (fromInt b)
-
--- add :: Int -> Int -> Int
--- add = natExp Add
-
--- mult :: Int -> Int -> Int
--- mult = natExp Mult
-
--- sub :: Int -> Int -> Int
--- sub = natExp Sub
+  NAnd :: Boolean -> Boolean -> Boolean
 
 
 
 
+natExp :: (Nat -> Nat -> Nat) -> Int -> Int -> Int
+natExp f a b = fromNat . eval $ f (fromInt a) (fromInt b)
 
-data IEq a = Equal a | NotEqual a deriving Show
-type Equality a = Expression (IEq a)
-data instance Expression (IEq a) where
-  Eq :: a -> a -> Equality a
-deriving instance (Show a, Show (IEq a)) => Show (Expression (IEq a))
+add :: Int -> Int -> Int
+add = natExp Add
 
--- instance Eval (Equality Nat) where
---   eval (Eq (Nat a) (Nat b)) = case (a, b) of
---     (Z, Z) -> undefined
+mult :: Int -> Int -> Int
+mult = natExp Mult
 
+sub :: Int -> Int -> Int
+sub = natExp Sub
 
-data IBoolean' :: * -> * where
-type Boolean' a = Expression (IBoolean' a)
-data instance Expression (IBoolean' a) where
-  T' :: a -> b -> Boolean' a
-  F' :: a -> b -> Boolean' b
+fromNat :: Nat -> Int
+fromNat n = fromNat' n 0
+  where fromNat' :: Nat -> Int -> Int
+        fromNat' Z acc = acc
+        fromNat' (S a) acc = fromNat' a (acc + 1)
+
+fromInt :: Int -> Nat
+fromInt n = fromInt' n Z
+  where fromInt' :: Int -> Nat -> Nat
+        fromInt' 0 acc = acc
+        fromInt' a acc = fromInt' (a - 1) (S acc)
